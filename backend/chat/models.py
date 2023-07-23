986a048b5c70e30c6f33b602b6835ae8b8ae9f71
Andrii <bezkrovnyyav@gmail.com>
2023-07-23 18:05:29 +0300

    Fixed models

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -5,17 +5,16 @@ from django_resized import ResizedImageField
 class CustomUser(models.Model):
     username = models.CharField(max_length=255, blank=False)
 
-    class Meta():
-        username = ['username']
+    def __str__(self):
+        return self.username
 
 
 class Room(models.Model):
     name = models.CharField(max_length=255, blank=False)
     image = ResizedImageField(force_format='WEBP', size=None,scale=0.5, quality=75, upload_to='images', blank=True, null=True)
 
-
-    class Meta:
-        room_name = ['name']
+    def __str__(self):
+        return self.name
 
 
 class Message(models.Model):
@@ -24,5 +23,5 @@ class Message(models.Model):
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     room = models.ForeignKey(Room,on_delete=models.CASCADE)
 
-    class Meta:
-        message_created = ['created_at']
+    def __str__(self):
+        return self.message_text
